daf6d93a873a5db6b42ce395b3525ba507cc5fa0
kanissh <kanissh11@gmail.com>
2020-10-23 17:52:47 +0530

    Added routes to bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ace_of_spades/home/home_screen.dart';
-import 'package:ace_of_spades/login/signin.dart';
+import 'package:ace_of_spades/profile/map_page.dart';
+import 'package:ace_of_spades/signin/signin.dart';
 import 'package:ace_of_spades/profile/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -8,7 +9,26 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> _bottomNavPages = [
+    HomePage(),
+    ProfilePage(),
+    MapPage(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onBottomItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,19 +44,22 @@ class MyApp extends StatelessWidget {
         fontFamily: 'OpenSans',
       ),
       home: SafeArea(
-          child: Scaffold(
-        body: ProfilePage(),
-        bottomNavigationBar: BottomNavigationBar(
-          iconSize: 24,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: [
-            BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.userAlt), label: 'Profile'),
-            BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.mapMarkedAlt), label: 'Map')
-          ],
+        child: Scaffold(
+          body: _bottomNavPages[_selectedIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            iconSize: 24,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: [
+              BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.userAlt), label: 'Profile'),
+              BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.mapMarkedAlt), label: 'Map')
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onBottomItemTapped,
+          ),
         ),
-      )),
+      ),
       routes: {
         HomePage.id: (context) => HomePage(),
         ProfilePage.id: (context) => ProfilePage(),
