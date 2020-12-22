db9247ba43236ba90ca19f07e75e3ce16acb77e6
kanissh <kanissh11@gmail.com>
2020-12-22 14:28:24 +0530

    Added basic Mapbox setup and basic scaffolding

--- lib/map/map_page.dart
+++ lib/map/map_page.dart
@@ -1,7 +1,8 @@
+import 'package:ace_of_spades/utils/config.helper.dart';
 import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
 class MapPage extends StatefulWidget {
-  static String id = '/mapPage';
   @override
   _MapPageState createState() => _MapPageState();
 }
@@ -9,9 +10,26 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Maps is Under Construction'),
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder(
+          future: loadConfigFile(),
+          builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+            print(snapshot.toString());
+            if (snapshot.hasData) {
+              return MapboxMap(
+                accessToken: snapshot.data['mapbox_api_token'],
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(45.45, 45.45),
+                ),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
       ),
     );
   }
