84f20a6a95c5a44e54f71c8fd89edf23b3273654
kanissh <kanissh11@gmail.com>
2021-01-04 22:23:12 +0530

    Added logic and data retrieval for class sch from firebase

--- lib/class_schedule/class_schedule_page.dart
+++ lib/class_schedule/class_schedule_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'course_schedule.dart';
@@ -11,6 +12,24 @@ class ClassSchedulePage extends StatefulWidget {
 class _ClassSchedulePageState extends State<ClassSchedulePage> {
   CalendarController _calendarController;
   List<CourseSchedule> courseScheduleList = <CourseSchedule>[];
+
+  CollectionReference classScheduleRef = FirebaseFirestore.instance.collection('class_schedule');
+  /* var doc = FirebaseFirestore.instance
+      .collection('students/s16/s16stu')
+      .doc(FirebaseAuth.instance.currentUser.email.substring(0, 3));
+ */
+
+  var studentDocument = FirebaseFirestore.instance.collection('students/s16/s16stu').doc('002');
+
+  List<String> getCourseCode(List list) {
+    List<String> results = List();
+
+    for (var item in list) {
+      results.add(item['course_code'].toString().toUpperCase());
+    }
+
+    return results;
+  }
 
   ScheduleDataSource _getCalendarDataSource() {
     courseScheduleList.add(CourseSchedule(
@@ -36,15 +55,48 @@ class _ClassSchedulePageState extends State<ClassSchedulePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SfCalendar(
-          controller: _calendarController,
-          timeSlotViewSettings: TimeSlotViewSettings(
-            startHour: 6,
-            endHour: 19,
-          ),
-          firstDayOfWeek: 1,
-          view: CalendarView.day,
-          dataSource: _getCalendarDataSource(),
+        body: FutureBuilder(
+          future: studentDocument.get(),
+          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Data error occurred'); //TODO: handle
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text('waiting'); //TODO: add waiting indicator
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data = snapshot.data.data();
+              List courseList = data['courses'];
+
+              // get courses where result is pending
+              List courseListPending = courseList.where((e) {
+                return e['grade'].toString().contains('pending');
+              }).toList();
+
+              return StreamBuilder(
+                stream: classScheduleRef.where('course_code', whereIn: getCourseCode(courseListPending)).snapshots(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    print('waiting');
+                  }
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    List list = List();
+                    print('active');
+                    snapshot.data.docs.map((e) {
+                      list.add(e.data());
+                    }).toList();
+
+                    print(list);
+                    print(list.length);
+                  }
+                  return Container();
+                },
+              );
+            }
+            return Text('Could not load data');
+          },
         ),
       ),
     );
