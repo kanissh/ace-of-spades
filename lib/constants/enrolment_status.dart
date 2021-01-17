01e5e69ec12ab9e8b7e0bdc07ad727180afd1410
kanissh <kanissh11@gmail.com>
2021-01-17 22:01:23 +0530

    Added constant files

---
+++ lib/constants/enrolment_status.dart
@@ -0,0 +1,6 @@
+class EnrolmentStatus {
+  static const String PROPER = 'proper';
+  static const String MEDICAL_PROPER = 'medical_proper';
+  static const String SPECIAL = 'special';
+  static const String REPEAT = 'repeat';
+}
