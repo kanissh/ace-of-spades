5f54e121f2f464d2dbdff58edfc8d6686db93e01
kanissh <kanissh11@gmail.com>
2021-01-18 22:57:08 +0530

    Added and tested add course logic with course remarks

--- lib/manage_courses/enrolment_service.dart
+++ lib/manage_courses/enrolment_service.dart
@@ -1,5 +1,6 @@
 import 'package:ace_of_spades/config/db.config.dart';
-import 'package:ace_of_spades/constants/enrolment_status.dart';
+import 'package:ace_of_spades/constants/course_status.dart';
+import 'package:ace_of_spades/constants/course_remarks.dart';
 import 'package:ace_of_spades/constants/grades.dart';
 import 'package:ace_of_spades/courses/course.dart';
 import 'package:ace_of_spades/grades/student_course.dart';
@@ -8,8 +9,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class EnrolmentService {
   CollectionReference _linkCollectionReference = FirebaseFirestore.instance.collection(DbConfigPath.CLASSROOM_LINK);
 
-  static addCourse(Course course) async {
-    String remarks = 'repeat';
+  static addCourse(Course course, String remarks) async {
     /* var studentDocument = FirebaseFirestore.instance
       .collection('${DbConfig.STUDENT}${FirebaseAuth.instance.currentUser.email.substring(0, 3).toLowerCase()}')
       .doc(FirebaseAuth.instance.currentUser.email.substring(3, 6).toString());
@@ -20,36 +20,7 @@ class EnrolmentService {
     var courseList =
         await studentDocument.get().then((DocumentSnapshot documentSnapshot) => documentSnapshot['courses']);
 
-    if (remarks == EnrolmentStatus.PROPER) {
-      if (courseList.any((element) => element['course_code'].toString().toUpperCase() == course.code.toUpperCase())) {
-        print('ERROR');
-      } else {
-        print('course added prop'); //add course;
-      }
-    } else if (remarks == EnrolmentStatus.MEDICAL_PROPER || remarks == EnrolmentStatus.SPECIAL) {
-      if (courseList.any((element) {
-        return element['course_code'].toString().toUpperCase() == course.code.toUpperCase() &&
-            (element['grade'] == Grades.O || element['grade'] == Grades.I);
-      })) {
-        print('course added'); //add course;
-      } else {
-        print('ERROR');
-      }
-    } else {
-      if (courseList.any((element) {
-        return element['course_code'].toString().toUpperCase() == course.code.toUpperCase() &&
-            (element['grade'] == Grades.C_MINUS ||
-                element['grade'] == Grades.D ||
-                element['grade'] == Grades.D_PLUS ||
-                element['grade'] == Grades.E);
-      })) {
-        print('course added'); //add course;
-      } else {
-        print('ERROR');
-      }
-    }
-
-    /* StudentCourse studentCourse = StudentCourse(
+    StudentCourse studentCourse = StudentCourse(
         code: course.code,
         name: course.name,
         credits: course.credits[course.credits.firstKey()],
@@ -59,10 +30,43 @@ class EnrolmentService {
         courseDocRef: FirebaseFirestore.instance
             .collection(DbConfigPath.COURSE)
             .doc(course.code.replaceAll(' ', '')), //FIXME: document names differ
-        status: CourseStatus.ENROLLED);
-    studentDocument.update({
-      'courses': FieldValue.arrayUnion([studentCourse.getMap()])
-    }); */
+        status: CourseStatus.ENROLLED,
+        remarks: remarks);
+
+    if (remarks == CourseRemarks.PROPER) {
+      if (courseList.any((element) => element['course_code'].toString().toUpperCase() == course.code.toUpperCase())) {
+        print('ERROR PROPER');
+      } else {
+        studentDocument.update({
+          'courses': FieldValue.arrayUnion([studentCourse.getMap()])
+        });
+      }
+    } else if (remarks == CourseRemarks.MEDICAL_PROPER || remarks == CourseRemarks.SPECIAL) {
+      if (courseList.any((element) {
+        return element['course_code'].toString().toUpperCase() == course.code.toUpperCase() &&
+            (element['grade'] == Grades.O || element['grade'] == Grades.I);
+      })) {
+        studentDocument.update({
+          'courses': FieldValue.arrayUnion([studentCourse.getMap()])
+        });
+      } else {
+        print('ERROR MP SP');
+      }
+    } else {
+      if (courseList.any((element) {
+        return element['course_code'].toString().toUpperCase() == course.code.toUpperCase() &&
+            (element['grade'] == Grades.C_MINUS ||
+                element['grade'] == Grades.D ||
+                element['grade'] == Grades.D_PLUS ||
+                element['grade'] == Grades.E);
+      })) {
+        studentDocument.update({
+          'courses': FieldValue.arrayUnion([studentCourse.getMap()])
+        });
+      } else {
+        print('ERROR R');
+      }
+    }
   }
 
   static removeCourse(StudentCourse studentCourse) {
