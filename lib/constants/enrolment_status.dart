ecb8cd2325e6ae7820756c27410fb9c06dfb19c5
kanissh <kanissh11@gmail.com>
2021-01-18 23:10:37 +0530

    File name change

--- lib/constants/enrolment_status.dart
+++
@@ -1,6 +0,0 @@
-class EnrolmentStatus {
-  static const String PROPER = 'proper';
-  static const String MEDICAL_PROPER = 'medical_proper';
-  static const String SPECIAL = 'special';
-  static const String REPEAT = 'repeat';
-}
