62126ac5d9f88f095f96ba158bfa9cf9ee68c7af
kanissh <kanissh11@gmail.com>
2021-01-30 19:08:19 +0530

    Added method to get smpt server

--- lib/utils/mail.helper.dart
+++ lib/utils/mail.helper.dart
@@ -4,9 +4,15 @@ import 'package:flutter/services.dart';
 import 'package:mailer/smtp_server.dart';
 
 const String _MAIL_CONFIG_PATH = 'assets/mail.json';
-SmtpServer smtpServer;
+SmtpServer _smtpServer;
 
 Future<SmtpServer> getSmptServer() async {
   String json = await rootBundle.loadString(_MAIL_CONFIG_PATH);
   var mailDetails = jsonDecode(json) as Map<String, dynamic>;
+
+  if (_smtpServer == null) {
+    return gmail(mailDetails['username'], mailDetails['password']);
+  } else {
+    return _smtpServer;
+  }
 }
