f5c105e6b6f8e47a36267e3a26285e6ed02f144e
kanissh <kanissh11@gmail.com>
2021-02-20 16:46:20 +0530

    Added under construction illustration

--- lib/constants/illustration_names.dart
+++ lib/constants/illustration_names.dart
@@ -3,3 +3,4 @@ const String illustrationAccessAccount = 'illustrations/access-account.svg';
 const String illustrationManage = 'illustrations/personal-settings-switch.svg';
 const String illustrationMap = 'illustrations/location-map.svg';
 const String illustrationInfo = 'illustrations/online-info.svg';
+const String illustrationUnderConstruction = 'illustrations/construction-workers.svg';
