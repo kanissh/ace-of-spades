e77381ba3f0b47621dc404954e02106cd8d6cc44
kanissh <kanissh11@gmail.com>
2021-02-26 13:16:40 +0530

    Debug banner set to false

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(
